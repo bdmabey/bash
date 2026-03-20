0525746ba0b90a8a998f478470ad1ac24ea13c26
Braden Mabey <braden.mabey@gmail.com>
2026-03-20 05:40:13 -0400

    Set up basic framework

--- nmap.sh
+++ nmap.sh
@@ -1,2 +1,20 @@
+#!/bin/bash
+# Sets up a couple of global arrays.
 declare -a CMD_LABELS=()
 declare -a CMD_LIST=()
+
+# Formatting helper functions
+section() { echo -e "\n== $1 =="; }
+
+collect_inputs() {
+	section "Target Configuration"
+	
+}
+
+main() {
+	clear
+	collect_inputs
+
+}
+
+main "$@"
